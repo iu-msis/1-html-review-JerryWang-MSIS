dcde1271f3da4bb88c0f62a195e6fea59021a4ad
JerryWang-MSIS <Pw9@iu.edu>
2021-09-21 21:04:39 -0400

    Testing with VueJS
    Attempting to fix issues with Vue JS

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -2,34 +2,61 @@
 The following code was included through follow professor Gregory's lecture on Wednesday
 */
 
-const app = Vue.createApp({
+const Offer = {
     data() {
         return {
             "person": {},
-            "profile": [
+            "offers": [
                 {
-                    "name": "Richard Hendricks"
-                    "countryoforigin":"USA"
-                    "birthdate":"April 10th"
-                    "age":"38"
-                    "email":"richardhendricks@piedpiper.com"
-                    "image":"img/richard.jpg"
+                    "name": "Richard Hendricks",
+                    "countryoforigin":"USA",
+                    "birthdate":"April 10th",
+                    "age":"38",
+                    "email":"richardhendricks@piedpiper.com",
+                    "image":"img/richard.jpg",
                 }
 
+                {
+                    "name": "Jerry Wang",
+                    "countryoforigin":"Taiwan",
+                    "birthdate":"April 9th",
+                    "age":"18",
+                    "email":"jerry@gmail.com",
+                    "image":"img/richard.jpg",
+                }
             ]
         }
+    },
+
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.person = parsedJson.results[0];
+            })
+            .catch( (err)=> {
+                console.error(err);
+        })
     }
-})
+},
+
+        created() {
+            this.fetchUserData();
+        }
+
+    }
 
 /*!
 The following code was included through follow professor Gregory's lecture on Wednesday
 */
-    created() {
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then((parsedJson) => {
-            this.person = parsedJson.result[0]})
-        .catch(err=> {
-            console.error(err)})
-    }
-}
+
+Vue.createApp(Offer).mount('#offerApp');
